41c3f2de9a3b95ec2c2404506cce48eca1f9896e
Kenji Ono <keno@riken.jp>
2014-02-11 21:08:37 +0900

    r27 add CC compiler wrapper script in configure.ac for NERSC Hopper [ver 1.4.6]

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 /*
  * TextParser - Text Parsing Library
  *
- * Copyright (C) 2012-2013 Institute of Industrial Science, The University of Tokyo.
+ * Copyright (C) 2012-2014 Institute of Industrial Science, The University of Tokyo.
  * All rights reserved.
  *
  */
@@ -14,8 +14,8 @@
 #ifndef _TEXTPARSER_VERSION_H_
 #define _TEXTPARSER_VERSION_H_
 
-#define TP_VERSION_NO  "1.4.5"
-#define TP_REVISION    "20131102_1430"
+#define TP_VERSION_NO  "1.4.6"
+#define TP_REVISION    "20140211_2100"
 
 #endif /* _TEXTPARSER_VERSION_H_ */
 
