3eabbd364f07aba05c6e83464cd4eaecb49b788f
fuchikamisatoshi <fuchikami@jp.fujitsu.com>
2014-02-21 18:18:50 +0900

    for windows

---
+++ include/Version.h
@@ -0,0 +1,21 @@
+/*
+ * TextParser - Text Parsing Library
+ *
+ * Copyright (C) 2012-2014 Institute of Industrial Science, The University of Tokyo.
+ * All rights reserved.
+ *
+ */
+
+/**
+ * @file   Version.h
+ * TextParserバージョン情報のヘッダーファイル
+ */
+
+#ifndef _TEXTPARSER_VERSION_H_
+#define _TEXTPARSER_VERSION_H_
+
+#define TP_VERSION_NO  "1.4.6"
+#define TP_REVISION    "20140211_2100"
+
+#endif /* _TEXTPARSER_VERSION_H_ */
+
